257b6535b43ef47161a21e9efb9bd85f376e2767
Mick Wonnink <Mick@micks-mac-pro.lan>
2015-10-16 09:46:45 -0700

    Initial Commit

---
+++ Cody/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Cody
+//
+//  Created by Mick Wonnink on 10/16/15.
+//  Copyright © 2015 nami. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
